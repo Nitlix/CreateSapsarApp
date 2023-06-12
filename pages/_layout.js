7cfb99dbb239c6effd7167209504bbf260433bc7
Nit <66027553+Nitlix@users.noreply.github.com>
2023-06-12 22:13:47 +0100

    Main

--- pages/_layout.js
+++ pages/_layout.js
@@ -1,25 +1,3 @@
-import { meta, title } from "sapsar/base"
-
-import useCSS from "sapsar/use/useCSS"
-import reportCSS from "sapsar/use/reportCSS"
-import useHead from "sapsar/use/useHead"
-
-import Combine from "sapsar/basic/Combine"
-
-
-useCSS([["flame.css"], ["global.css"]])
-reportCSS("*", ["global"])
-
-useHead('*', 
-    Combine(
-        title("Sapsar"),
-        meta({
-            name: "description",
-            content: "Sapsar is a lightspeed web framework designed for dynamically rendered pages using components.s"
-        })
-    )
-)
-
 export default async function Layout(content, data, page){
     return (
         content
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
-import Combine from "sapsar/basic/Combine"
-import useComponent from "sapsar/use/useComponent"
-
-import { p } from "sapsar/base"
-
-const Header = await useComponent("index", "global/header")
-
+import { h1 } from "sapsar/base"
 
 
 export default async function Page(data){    
     return (
-        Combine(
-            Header(
-                "index"
-            ),
-            p(
-                `It's e!`
-            )
+        h1(
+            "Hello World"
         )
     )
 }
