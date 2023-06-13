c2714a3dd64fb6d4fafec5f3748f1a2ecdfedfa4
Nit <66027553+Nitlix@users.noreply.github.com>
2023-06-13 11:11:58 +0100

    Final

--- pages/_layout.js
+++ pages/_layout.js
@@ -1,3 +1,23 @@
+import useCSS from "sapsar/use/useCSS"
+import reportCSS from "sapsar/use/reportCSS"
+
+import useHead from "sapsar/use/useHead"
+import title from "sapsar/base"
+
+
+useCSS([["global.css"]])
+reportCSS("*", ["global"])
+
+
+useHead(
+    '*', 
+    Combine(
+        title(
+            "Sapsar"
+        )
+    )
+)
+
 export default async function Layout(content, data, page){
     return (
         content
